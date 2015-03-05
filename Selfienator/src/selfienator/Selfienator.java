37247ee6c11958bf2c44c6641107870723d708df
MatejD <mdujava@gmail.com>
2015-03-05 19:31:43 +0100

    first commit

--- Selfienator/src/selfienator/Selfienator.java
+++ Selfienator/src/selfienator/Selfienator.java
@@ -6,65 +6,59 @@
 package selfienator;
 
 import javafx.application.Application;
-import static javafx.application.Application.launch;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.*;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-
 /**
  * my Main class
+ * 
  * @author Matej
  */
-public class Selfienator extends Application
-{
-	private StringProperty textPrvy;
-	
-	public string getTextPrvy()
-    {
-        return textPrvy.get();
-    }
+public class Selfienator extends Application {
+	private SimpleStringProperty textPrvy;
 
-    public void setTextPrvy(string value)
-    {
-    	textPrvy.set(value);
-    }
-	
-    public StringProperty textPrvyProperty() {
-        return textPrvy;
-    }
-    
+	public String getTextPrvy() {
+		return textPrvy.get();
+	}
+
+	public void setTextPrvy(String value) {
+		textPrvy.set(value);
+	}
+
+	public StringProperty textPrvyProperty() {
+		return textPrvy;
+	}
+
 	/**
 	 * Starts my app
 	 */
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
-    
-        Scene scene = new Scene(root, 300, 275);
-    
-        stage.setTitle("Selfienator");
-        stage.setScene(scene);
-        
-        setTextPrvy("Ahoj");
-        
-        stage.show();
-    }
-	
+	public void start(Stage stage) throws Exception {
+
+		textPrvy = new SimpleStringProperty();
+
+		setTextPrvy("Ahoj");
+
+		Parent root = FXMLLoader
+				.load(getClass().getResource("MainWindow.fxml"));
+
+		Scene scene = new Scene(root, 300, 275);
+
+		stage.setTitle("Selfienator");
+		stage.setScene(scene);
+
+		stage.show();
+	}
+
 	/**
-	 * @param args the command line arguments
+	 * @param args
+	 *            the command line arguments
 	 */
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 }
